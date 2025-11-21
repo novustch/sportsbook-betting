6e0cd17ebe95b58efc8e21c0b774dca53cee4b6b
novustch <mirakulizicc@gmail.com>
2025-11-21 07:03:09 -0500

    feat: add admin

---
+++ src/layout/AdminPanel/index.tsx
@@ -0,0 +1,6 @@
+/**
+ * This is the free/public version of the project. The full Admin Panel source
+ * code is intentionally not shared here. If you’d like the complete admin
+ * build or bespoke development, please contact me via the channels listed in
+ * the README.
+ */
